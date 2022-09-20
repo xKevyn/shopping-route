782f56d6bb533516cec55b2a723cae4025b8b512
Kevyn <42858159+xKevyn@users.noreply.github.com>
2022-09-20 23:53:32 +0800

    added node and path class

--- ant_colony.py
+++ ant_colony.py
@@ -1,2 +1,29 @@
-
-print("Hello World)
+class Node:
+    def __init__(self, name, category, floor):
+        self.name = name
+        self.category = category
+        self.floor = floor
+    
+class Path:
+    def __init__(self, node1, node2, distance, time, stamina):
+        self.node1 = node1
+        self.node2 = node2
+        self.distance = distance
+        self.time = time
+        self.stamina = stamina
+        
+class AntColony:
+    def __init__(self, paths):
+        self.paths = paths
+        
+    def initialisation():
+        ...
+        
+if __name__ == "__main__":
+    state_space = [
+        Path(Node('a', 't', 1), Node('b', 't', 1), 1, 1, 1), 
+        Path(Node('a', 't', 1), Node('c', 't', 1), 2, 2, 3), 
+        Path(Node('a', 't', 1), Node('d', 't', 1), 3, 3, 3)
+        ]
+    
+    print(state_space[0].node1.name)
