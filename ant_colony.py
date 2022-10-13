9494d352c0b61a6f41dcfa0243db0587c15fb9f5
xBigfish <114007278+xBigfish@users.noreply.github.com>
2022-10-13 20:34:38 +0800

    Update ant_colony.py
    added comment lines

--- ant_colony.py
+++ ant_colony.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 class Node:
     def __init__(self, name, category, floor):
-        self.name = name
-        self.category = category
+        self.name = name #the name of the shop/entrance/stairs/lifts
+        self.category = category 
         self.floor = floor
-        self.roads = []
-        self.coordinates = []
+        self.roads = [] #road connected to the current node
+        self.coordinates = [] #(x,y)
     
     def set_coordinates(self, coordinates):
         self.coordinates = coordinates
@@ -17,12 +17,12 @@ class Node:
           self.roads.append(path)
         
 class Road:
-    def __init__(self, connected_nodes, distance, time, stamina, pheromone=0): # or random small number
+    def __init__(self, connected_nodes, distance, time, stamina, pheromone=0): #initial pheromone set to 0
         self.connected_nodes = connected_nodes
-        self.distance = distance
-        self.time = time
-        self.stamina = stamina
-        self.pheromone = pheromone
+        self.distance = distance #the walking distance of this road
+        self.time = time #the time spent on walking this road
+        self.stamina = stamina #the stamina used on walking this road
+        self.pheromone = pheromone 
         
     def set_pheromone(self, pheromone):
         self.pheromone = pheromone
@@ -31,31 +31,35 @@ class Road:
     # update the pheromone of the path
         self.pheromone = (1-rho)*self.pheromone
     
+    #function to deposit pheromone for distance only scenario 
     def deposit_pheromone_distance(self, ants):
     # 1. search for ants that uses the raod
     # 2. deposit pheromone using the inversely proportionate relationship between path length and deposited pheromone
+        deposited_pheromone = 0
         for ant in ants:
-            for path in ant.path:
-                if self == path:
-                    self.pheromone += 5/(ant.get_path_distance())**1
-                    break
+            if self in ant.path:
+                deposited_pheromone += 5/(ant.get_path_distance())**1
+        self.pheromone += deposited_pheromone
                 
+   #function to deposit pheromone when considering all factors 
     def deposit_pheromone_all_cost(self, ants):
+        deposited_pheromone = 0
         for ant in ants:
-            for path in ant.path:
-                if self == path:
-                    self.pheromone += 5/(ant.get_path_all_cost())**1
-
+            if self in ant.path:
+                deposited_pheromone += 5/(ant.get_path_all_cost())**1
+        self.pheromone += deposited_pheromone
+                    
 class Ant:
     def __init__(self):
       self.nodes = [] # nodes the ant passes through, in sequence
       self.path = [] # roads the ant uses, in sequence
     
     def get_path(self, origin, destination, alpha, previous_nodes):
-    # 1. append origin to the self.nodes
-    # 2. if the last node is not destination, search for the next node to go
-    # 3. after getting to the destination, remove the loop within the path, i.e. if there are repeated nodes in self.nodes, remove the nodes and the roads in between the repetition
-        self.nodes.append(origin)
+  
+        self.nodes.append(origin)  # append origin to the self.nodes
+        
+        #if (the last node is not destination) or (is in the previous_nodes), search for the next node to go
+        #the XOR logic is used because the destination entered by the user can be node(shop) or category(string)
         while(not((self.nodes[-1] == destination) ^ (self.nodes[-1].category == destination)) or self.nodes[-1] in previous_nodes): 
             if len(self.path) > 0:
                 available_roads = [r for r in self.nodes[-1].roads if r is not self.path[-1]]
@@ -76,6 +80,7 @@ class Ant:
             else:
                 self.nodes.append(self.path[-1].connected_nodes[0])
             
+        #remove the loop
         while len(set(self.nodes)) != len(self.nodes):
             for i, node in enumerate(set(self.nodes)):
                 node_indices = [i for i, x in enumerate(self.nodes) if x == node]
@@ -83,15 +88,16 @@ class Ant:
                     self.nodes = self.nodes[:node_indices[0]] + self.nodes[node_indices[-1]:]
                     self.path = self.path[:node_indices[0]] + self.path[node_indices[-1]:]
                     break
-        return self.nodes[-1]
+        return self.nodes[-1] #return the last node
     
+    #function to get the total path distance
     def get_path_distance(self):
-        path_distance = sum([i.distance for i in self.path])
+        path_distance = sum([i.distance for i in self.path]) # calculate path distance based on self.path
         return path_distance
-    # calculate path distance based on self.path
-       # return path_distance
-       
+    
+    #function to get the total cost include distance, time and stamina   
     def get_path_all_cost(self):
+        # calculate the sum of path distance, time and stamina based on self.path
         path_all_cost = sum([i.distance for i in self.path]) + sum([i.time for i in self.path]) + sum([i.stamina for i in self.path])
         return path_all_cost
     
@@ -121,27 +127,31 @@ def get_percentage_of_dominant_path(ants):
         percentage = max(frequencies)/sum(frequencies)
     return percentage
 
+#function to plot the graph
 def create_graph(nodes, fitness_all_cost):
     plt.figure()
+    
+    #check whether the algorithm is based on distance only or all cost, display different title accordingly
     if fitness_all_cost:
-        plt.suptitle("Optimal Path based on distance, time and stamina")
+        plt.suptitle("Optimal Path based on distance, time and stamina") 
     else:  
         plt.suptitle("Optimal Path based on distance only")
-    ax = plt.axes(projection='3d')
+    ax = plt.axes(projection='3d') #graph will be plotted in 3D
     nodes_x = [node.coordinates[0] for key, node in nodes.items()]
     nodes_y = [node.coordinates[1] for key, node in nodes.items()]
-    nodes_z = [node.floor for key, node in nodes.items()]
+    nodes_z = [node.floor for key, node in nodes.items()] #z-axis is indicating the floor of the nodes
     ax.scatter(nodes_x, nodes_y, nodes_z)
     for i, node in enumerate(nodes):
         ax.text(nodes_x[i], nodes_y[i], nodes_z[i], node, size=7, color="k")
     return ax
 
+#function to draw the line of each path on the graph
 def draw_path(ax, solution):
     lines = []
     colors = []
     count = 0
     for i in range(len(solution)):
-        colors.append('#%06X' % random.randint(0, 0xFFFFFF))
+        colors.append('#%06X' % random.randint(0, 0xFFFFFF)) #different colour line is drawn for every path
     for path in solution:
         for road in path:
             from_coord = road.connected_nodes[0].coordinates
@@ -158,9 +168,9 @@ def aco(iteration,
         ants,
         origin,
         destination,
-        fitness_all_cost,
-        previous_nodes,
-        max_iteration=200,
+        fitness_all_cost, #a boolean variable to determine this algorithm is distance only or all cost 
+        previous_nodes, #a list that stores the previous nodes/shops which has been choosen before by the user
+        max_iteration=200, 
         percentage_of_dominant_path=0.9,
         alpha=1,
         rho=0.1):
@@ -184,14 +194,16 @@ def aco(iteration,
         # increase iteration count
         iteration += 1
     
-    return destination
+    return destination #return the destination of the path
 
+#function to get the list of shop/category to visit from user input 
 def get_user_input(location_list, nodes):
     categories = [node[1] for node in location_list]
     category_list = []
     [category_list.append(item) for item in categories if item not in category_list]
-    category_list = category_list[:-3]
+    category_list = category_list[:-3] #creating a list that stores the categories' name
     
+    #a dictionary which indicating the count of each category
     category_count = {"Digital & Home Appliances": 2,
                   "Food & Beverages": 3,
                   "Supermarket": 2,
@@ -203,6 +215,7 @@ def get_user_input(location_list, nodes):
                   "Lifestyle & Home Living": 1
                   }
     
+    #a dictionary which indicating the current count of each category, it will be added 1 whenever user choose the category or shop from the category
     current_category_count = {"Digital & Home Appliances": 0,
                   "Food & Beverages": 0,
                   "Supermarket": 0,
@@ -214,10 +227,12 @@ def get_user_input(location_list, nodes):
                   "Lifestyle & Home Living": 0
                   }
     
-    shop_list = [node[0] for node in location_list]
-    entrance_list = shop_list[-12:-6]
-    shop_list = shop_list[:-12]
     
+    shop_list = [node[0] for node in location_list]
+    entrance_list = shop_list[-12:-6] #creating a list of entrance
+    shop_list = shop_list[:-12] #creating a list that stores the name of the shops
+    
+    #print out every of entrances, shops, and categories to the user
     print("---------- Entrances --------")
     [print(entrance) for entrance in entrance_list]
     
@@ -227,58 +242,72 @@ def get_user_input(location_list, nodes):
     print("---------- Categories ----------")
     [print(category) for category in category_list]
       
-    origin = input("Current location: ")
+    origin = input("Current location: ") #prompt user to input their current location
     
+    #only proceed when the user enter a valid entrance or shop on your current location
     while origin not in shop_list and origin not in entrance_list:
         print("Please enter a valid entrance or shop on your current location")
         origin = input("Current location: ")
+        
+    destination_list = [] #destination_list is used to store the shop(nodes) or category(string) that the user wanted to go
+    current_input = [] #current_input is used to store the user input in string format 
     
-    destination_list = [] 
-    current_input = []
-    
-    destination_list.append(nodes[origin])
+    destination_list.append(nodes[origin]) 
     current_input.append(origin)
     
     in_process = True
     
+    #process on asking the user to enter the shop he wanted to visit
     while in_process:
         destination = input("Please enter your shop or category to visit or an exit: ")
         print()
-        if destination not in shop_list and destination not in category_list and destination not in entrance_list:
-            print("Please enter a valid input")
-            
+        
+        #if user enter an invalid input
+        if destination not in shop_list and destination not in category_list and destination not in entrance_list: 
+            print("Please enter a valid input") 
+             
+        #if user enter a shop
         if destination in shop_list:
+            #only proceeed if the shop is not selected before yet
             if destination not in current_input and current_category_count[nodes[destination].category] != category_count[nodes[destination].category]:
-                destination_list.append(nodes[destination])
+                destination_list.append(nodes[destination]) #create a nodes object using the shop name and append it into the destination list
                 current_input.append(destination)
-                current_category_count[nodes[destination].category] += 1
+                current_category_count[nodes[destination].category] += 1 #added 1 to the number mapped on the shop's category in the current_category_count dictionary list
             else:
                 print("The destination has been selected before.")
+                
+       #if user enter a category
         if destination in category_list:
+            #only proceed if there is still remaining shop there are not been chosen yet
+            #to check this, compare the number mapped on the category in the two dictionary list, either is same or not
             if current_category_count[destination] != category_count[destination]:
-                destination_list.append(destination)
+                destination_list.append(destination) #no need to create nodes object because category should be string variable
                 current_input.append(destination)
-                current_category_count[destination] += 1
+                current_category_count[destination] += 1 #added 1 to number mapped on the category in the current_category_count dictionary list
             else:
                 print("There is no remaining shop in this category.")
+                
+        #if user enter an exit
         if destination in entrance_list:
             destination_list.append(nodes[destination])
             current_input.append(destination)
             print("Current input:", current_input)
-            break
+            break #break the loop because the user entered an exit, which means he is done
         
         print("Current input:", current_input)
         
-        if input("Do you wish do exit? (Y/N) ").upper() == "Y":
+        if input("Do you wish do exit? (Y/N) ") == "Y": #instead of entering exit, the user can enter "Y" to exit
             in_process = False
     
-            
-    if current_input[-1][0] != 'E' or current_input[-1][2] != '-':
-        destination_list.append("Entrance")
+    #if the last current_input is not entrance, which means the user did not choose an exit
+    if current_input[-1] not in entrance_list:
+        #append the category "Entrance" into the destination_list, so the algorithm will choose an exit for the user with best route
+        destination_list.append("Entrance") 
         
     return destination_list
 
-def final_aco(shop_list, fitness_all_cost):
+#this function is used to track aco algorithm running on the destination_list entered by the user
+def final_aco(destination_list, fitness_all_cost): 
     n_ant = 20
     
     initial_pheromone = 0.01
@@ -287,25 +316,31 @@ def final_aco(shop_list, fitness_all_cost):
     
     iteration = 0
     
-    solutions = []
-    final_paths = []
-    previous_nodes = []
+    solutions = [] #a list used to store the sequence of the nodes in the optimal path 
+    final_paths = [] #a list used to store the sequence of the road in the optimal path
+    previous_nodes = [] #a list used to stored the desired shop that been visited previously
     
-    for i in range(len(shop_list)-1):
+    for i in range(len(destination_list)-1):
         for road in roads:
             road.set_pheromone(initial_pheromone)
-        destination = aco(iteration, roads, ants, shop_list[i], shop_list[i+1], fitness_all_cost, previous_nodes)
-        shop_list[i+1] = destination
+        
+        #perform aco algorithm in every two elements from the destination_list
+        destination = aco(iteration, roads, ants, destination_list[i], destination_list[i+1], fitness_all_cost, previous_nodes)
+        
+        #since aco() function will return a node object, the destination_list[i+1] will be changed to the destination node returned from the aco function
+        #this is to make sure the destination_list[i] is node instead of string in next iteration
+        destination_list[i+1] = destination
         
         [freq, paths, nodes_used] = get_frequency_of_paths(ants)
-        final_paths.append(paths[freq.index(max(freq))])
-        solutions.append([n.name for n in nodes_used[freq.index(max(freq))]])
+        final_paths.append(paths[freq.index(max(freq))]) 
+        solutions.append([n.name for n in nodes_used[freq.index(max(freq))]]) 
         previous_nodes.append(nodes[solutions[-1][-1]])
         for ant in ants:
             ant.reset()
         
-    return shop_list, solutions, final_paths
+    return destination_list, solutions, final_paths
 
+#function used to arrange the solutions list return from the final_aco for a better visualization
 def print_solution(solutions):        
     for i, solution in enumerate(solutions):
         if(i != len(solutions)-1):
@@ -411,9 +446,9 @@ if __name__ == "__main__":
         # transition between floor
         ["S-1","S-2",50, 100, 100],
         ["S-2","S-3",50, 100, 100],
-        ["L-1","L-2",50, 25, 0],
-        ["L-2","L-3",50, 25, 0],
-        ["L-1","L-3",50, 30, 0],
+        ["L-1","L-2",0, 20, 0],
+        ["L-2","L-3",0, 20, 0],
+        ["L-1","L-3",0, 30, 0],
         ["Uniqlo-2","Uniqlo-3",50, 100, 100]
         ]
     
@@ -430,24 +465,36 @@ if __name__ == "__main__":
       
     nodes_list = get_user_input(location_list, nodes) 
     
+    #plotting two graphs with different considering factor for optimal paths
     ax = create_graph(nodes, False)
     ax2 = create_graph(nodes, True)
     
+    #performing two algorithm with different considering factor for optimal paths
     shop_list_distance_only, solution_distance_only, path_distance_only = final_aco(nodes_list.copy(), False)
     shop_list_all_cost, solution_all_cost, path_all_cost = final_aco(nodes_list.copy(), True)
     
     print()
+    
+    #printing the sequence of the desired places travelled on the optimal path (considering distance factor only)
     print("Places to go considering only distance:")
     print([node.name for node in shop_list_distance_only])
     print()
+    
+    #printing the sequence of all the places travelled on the optimal path (considering distance factor only)
     print("Path to go considering only distance:")
     print_solution(solution_distance_only)
     print()
+
+    #printing the sequence of the desired places travelled on the optimal path (considering every factors)
     print("Places to go considering distance, time, stamina:")
     print([node.name for node in shop_list_all_cost])
     print()
+    
+    #printing the sequence of all the places travelled on the optimal path (considering every factors)
     print("Path to go considering distance, time, stamina:")   
     print_solution(solution_all_cost)
     
+    #visualize the graph
     draw_path(ax, path_distance_only)
     draw_path(ax2, path_all_cost)
+
